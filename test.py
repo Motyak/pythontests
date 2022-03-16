86f647c65656b886060c98cd7d1481f7dfeb422f
Tommy Anfosso <anfossotommy@hotmail.fr>
2022-03-16 20:25:33 +0100

    start

---
+++ test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from re import match
+from os import listdir
+from sys import stderr
+
+from yearprogress import yearprogress
+FUNCTION_TO_TEST = yearprogress
+
+TESTS_PATH = './tests/'
+
+def test():
+    test_files = listdir(TESTS_PATH)
+
+    if not test_files:
+        return
+
+    tests = []
+    for fname in test_files:
+        m = match(r'(.*)_out\.txt', fname)
+        if m:
+            tests.append(m.group(1))
+
+    if not tests:
+        return
+        
+    for t in tests:
+        input = open(TESTS_PATH + t + '.txt', encoding='utf8').read()
+        expecting = open(TESTS_PATH + t + '_out.txt', encoding='utf8').read()
+        output = FUNCTION_TO_TEST(input)
+
+        if output != expecting:
+            print(
+                f"Test '{t}' is failing:", 
+                "\n-Expected: ", r"'''", f"{expecting}", r"'''", 
+                "\n-Had: ", r"'''", f"{output}", r"'''",
+                file=stderr, sep=''
+            )
+            exit(1)
+
+test()
---
+++ yearprogress.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from sys import stdin
+
+def yearprogress(input):
+    return input
+
+if __name__ == "__main__":
+    userInput = stdin.read()
+    print(yearprogress(userInput), end='')
