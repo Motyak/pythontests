26ade9a795e5bc2618b1371d909c1627f7889803
Motyak <m0tyak_@live.fr>
2022-03-17 17:53:25 +0100

    added manual test

--- test_today.py
+++ test_today.py
@@ -3,11 +3,9 @@
 from unittest import mock, TestCase, main
 from datetime import datetime
 import requests # get()
+import sys # stderr
 
 import today # _now()
-
-class MockDatetime:
-    pass
 
 class TodayTest(TestCase):
     @classmethod
@@ -19,14 +17,28 @@ class TodayTest(TestCase):
         mockDatetime.now.return_value = self.customDate
         assert today._now() == self.customDate
 
-    def testDatetimeNow(self):
+    def testDatetimeNowMatchesSystemTime(self):
+        print(datetime.now())
+        userInput = input('Does this match your system clock? (Y/n)\n>')
+        assert not userInput or userInput.lower() == 'y'
+
+    def testSystemTimeMatchesWebTime(self):
         API_URL = 'http://worldclockapi.com/api/json/utc/now'
         API_FIELD = 'currentDateTime'
         API_FIELD_FORMAT = r'%Y-%m-%dT%H:%MZ'
-        res = requests.get(url=API_URL).json()
-        webDateAsStr = res[API_FIELD]
+
         dateAsStr = datetime.utcnow().strftime(API_FIELD_FORMAT)
-        assert webDateAsStr == dateAsStr
+        try:
+            res = requests.get(url=API_URL).json()
+            webDateAsStr = res[API_FIELD]
+            if dateAsStr != webDateAsStr:
+                print(f"WARN: Your system clock ({dateAsStr}) isn't on time, "
+                        + f'should be {webDateAsStr}', file=sys.stderr)
+        except:
+            print(f"WARN: We couldn't check if your system clock is on time "
+                    + f'({dateAsStr})', file=sys.stderr)
+
+        assert True
 
 if __name__ == '__main__':
     main()
