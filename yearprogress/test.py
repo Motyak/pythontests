09052fd34456b1986f407a8377e34cda5d58abe0
Tommy Anfosso <anfossotommy@hotmail.fr>
2022-03-20 20:05:11 +0100

    fixed test.py

--- yearprogress/test.py
+++ yearprogress/test.py
@@ -29,7 +29,7 @@ def test():
         expecting = open(TESTS_PATH + t + '_out.txt', encoding='utf8').read()
         output = FUNCTION_TO_TEST(input)
 
-        if output != expecting:
+        if str(output) != expecting:
             print(
                 f"Test '{t}' is failing:", 
                 "\n-Expected: ", r"'''", f"{expecting}", r"'''", 
