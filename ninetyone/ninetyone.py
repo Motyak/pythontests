6bd7b16729850fc318679bb1485d22e02a5f9335
Tommy Anfosso <anfossotommy@hotmail.fr>
2022-03-20 18:22:20 +0100

    start ninetyone

---
+++ ninetyone/ninetyone.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from sys import stdin
+
+def ninetyone(input):
+    return input
+
+if __name__ == "__main__":
+    userInput = stdin.read()
+    print(ninetyone(userInput), end='')
---
+++ ninetyone/test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import re # match()
+import os # listdir()
+import sys # stderr
+
+from yearprogress import yearProgressFromStr
+FUNCTION_TO_TEST = yearProgressFromStr
+
+TESTS_PATH = './examples/'
+
+def test():
+    test_files = os.listdir(TESTS_PATH)
+
+    if not test_files:
+        return
+
+    tests = []
+    for fname in test_files:
+        m = re.match(r'(.*)_out\.txt', fname)
+        if m:
+            tests.append(m.group(1))
+
+    if not tests:
+        return
+        
+    for t in tests:
+        input = open(TESTS_PATH + t + '.txt', encoding='utf8').read()
+        expecting = open(TESTS_PATH + t + '_out.txt', encoding='utf8').read()
+        output = FUNCTION_TO_TEST(input)
+
+        if output != expecting:
+            print(
+                f"Test '{t}' is failing:", 
+                "\n-Expected: ", r"'''", f"{expecting}", r"'''", 
+                "\n-Had: ", r"'''", f"{output}", r"'''",
+                file=sys.stderr, sep=''
+            )
+            exit(1)
+
+test()
